e674716e508cbe670ffb09bea8a75371b184e41e
mwanjiku56 <mwanjiku56@gmail.com>
2021-12-14 18:38:57 +0300

    Update file

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,11 +3,9 @@
 Locked Class
 """
 
-
 class LockedClass:
     """ Prevent user from creating new attributes """
     __slots__ = ['first_name']
 
     def __init__(self, first_n=""):
         self.first_name = first_n
-        
