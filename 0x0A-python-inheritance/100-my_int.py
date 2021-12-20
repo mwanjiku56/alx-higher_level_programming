ce01c10936008211358b1f8daf4c6a0710a4ad3c
mwanjiku56 <mwanjiku56@gmail.com>
2021-12-21 01:59:14 +0300

    Add new files

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python3
+""" Program that inherits from int class """
+
+
+class MyInt(int):
+    """ class MyInt that inherits from int """
+    def __eq__(self, other):
+        """ equal (=) inverted """
+        return False
+
+    def __ne__(self, other):
+        "not equal (!=) inverted """
+        return True
+        
